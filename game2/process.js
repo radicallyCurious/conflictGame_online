2a7979e03ca1eb89196329c5f073008432e33db4
Luc Pitre <pitreluc@gmail.com>
2016-12-14 22:32:57 -0500

    pdating, need canvas saved as image

--- game2/process.js
+++ game2/process.js
@@ -3,47 +3,40 @@
 *
 *
 */
-
-var pic1Loc = "person1.jpg";//location of person1 picture
-var pic2Loc = "person2.jpg";//location of person2 picture
-
-var img1;
-var img2;
-
 var canv = document.getElementById("canv");
 var context = canv.getContext("2d");
 
 //create array of strings
 
-var strings = ["", "", "", "", "", ""];
-
 //take in 6 textarea.values as strings
 
-/*function pull_in(){
-	
-			console.log("working!");
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+        var cars = text.split("\n");
 
-			/*strings[0] = document.getElementById("textD").value;
-			strings[1] = document.getElementById("textC").value;
-			strings[2] = document.getElementById("conD").value;
-			strings[3] = document.getElementById("conC").value;
-			strings[4] = document.getElementById("reaD").value;
-			strings[5] = document.getElementById("reaC").value;
-			//grab image
-			img1 = new Image();
-			img2 = new Image();
+        for (var ii = 0; ii < cars.length; ii++) {
 
-			img1.src = pic1Loc;
-			img2.src = pic2Loc;
+            var line = "";
+            var words = cars[ii].split(" ");
 
-			img1.onLoad = function(){
-				context.drawImage(img1, 100, 100);
-			}
-			img2.onLoad = function(){
-				context.drawImage(img2, 400, 100);
-			}*/
+            for (var n = 0; n < words.length; n++) {
+                var testLine = line + words[n] + " ";
+                var metrics = context.measureText(testLine);
+                var testWidth = metrics.width;
 
-		}//end pull_in()
+                if (testWidth > maxWidth) {
+                    context.fillText(line, x, y);
+                    line = words[n] + " ";
+                    y += lineHeight;
+                }
+                else {
+                    line = testLine;
+                }
+            }
+
+            context.fillText(line, x, y);
+            y += lineHeight;
+        }
+     }
 
 
 //make circles for dialogue
